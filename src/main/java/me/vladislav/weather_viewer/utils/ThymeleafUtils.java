41faba758a940c87dc64c3dfd47eec758e39dd6a
vladislavmedvedev <EnoT3581@mail.ru>
2024-04-28 15:42:19 +0300

    feat: added ThymeleafUtils.java

---
+++ src/main/java/me/vladislav/weather_viewer/utils/ThymeleafUtils.java
@@ -0,0 +1,24 @@
+package me.vladislav.weather_viewer.utils;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+public class ThymeleafUtils {
+
+    private WebContext getWebContext(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext){
+        JakartaServletWebApplication application =
+                JakartaServletWebApplication.buildApplication(servletContext);
+
+        IWebExchange iWebExchange = application.buildExchange(request, response);
+
+        WebContext webContext = new WebContext(iWebExchange);
+
+        return webContext;
+    }
+
+}
