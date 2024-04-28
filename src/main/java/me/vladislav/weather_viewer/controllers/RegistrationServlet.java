638b8d3c146e29c5665aa088b2003ad2917d46c4
vladislavmedvedev <EnoT3581@mail.ru>
2024-04-28 16:32:22 +0300

    refactor: updated RegistrationServlet.java
    added template processing

--- src/main/java/me/vladislav/weather_viewer/controllers/RegistrationServlet.java
+++ src/main/java/me/vladislav/weather_viewer/controllers/RegistrationServlet.java
@@ -1,2 +1,28 @@
-package me.vladislav.weather_viewer.controllers;public class RegistrationServlet {
+package me.vladislav.weather_viewer.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import me.vladislav.weather_viewer.utils.ThymeleafUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import java.io.IOException;
+
+@WebServlet(name = "RegistrationServlet", value = "/registration")
+public class RegistrationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebContext webContext = ThymeleafUtils.getWebContext(req, resp, getServletContext());
+
+        TemplateEngine templateEngine = (TemplateEngine)(getServletContext().getAttribute("templateEngine"));
+
+        templateEngine.process("registration.html", webContext, resp.getWriter());
+
+        req.getRequestDispatcher("registration.html").forward(req, resp);
+    }
+
+
 }
