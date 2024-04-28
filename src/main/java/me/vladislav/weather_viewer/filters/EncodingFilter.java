44af95ff9a16b4dfb4b33b7a868ff0cdc81c0bdb
vladislavmedvedev <EnoT3581@mail.ru>
2024-04-28 16:24:09 +0300

    feat: added EncodingFilter.java

---
+++ src/main/java/me/vladislav/weather_viewer/filters/EncodingFilter.java
@@ -0,0 +1,24 @@
+package me.vladislav.weather_viewer.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter
+public class EncodingFilter extends HttpFilter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.addHeader("Content-type", "application/json;charset=UTF-8");
+        filterChain.doFilter(request, response);
+    }
+}
