0676c587f5a32649c3884f437cb24b15c735dbff
vladislavmedvedev <EnoT3581@mail.ru>
2024-04-29 20:53:32 +0300

    refactor: updated EncodingFilter.java

--- src/main/java/me/vladislav/weather_viewer/filters/EncodingFilter.java
+++ src/main/java/me/vladislav/weather_viewer/filters/EncodingFilter.java
@@ -2,8 +2,6 @@ package me.vladislav.weather_viewer.filters;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,13 +9,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebFilter
+@WebFilter(filterName = "EncodingFilter")
 public class EncodingFilter extends HttpFilter {
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         response.addHeader("Content-type", "application/json;charset=UTF-8");
         filterChain.doFilter(request, response);
     }
