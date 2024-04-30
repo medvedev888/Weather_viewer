2d047199aa1ba1bcd8f3496e1808664dee2d2c16
vladislavmedvedev <EnoT3581@mail.ru>
2024-04-30 21:53:59 +0300

    refactor: updated DataAccessObject.java, UserDAO.java

--- src/main/java/me/vladislav/weather_viewer/dao/DataAccessObject.java
+++ src/main/java/me/vladislav/weather_viewer/dao/DataAccessObject.java
@@ -1,2 +1,11 @@
-package me.vladislav.weather_viewer.dao;public class DataAccessObject {
+package me.vladislav.weather_viewer.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DataAccessObject<T> {
+    public Optional<T> getById(int id);
+    public Optional<List<T>> getAll();
+    public void save(T t);
+    public void delete(T t);
 }
--- src/main/java/me/vladislav/weather_viewer/dao/UserDAO.java
+++ src/main/java/me/vladislav/weather_viewer/dao/UserDAO.java
@@ -1,2 +1,76 @@
-package me.vladislav.weather_viewer.dao;public class UserDAO {
+package me.vladislav.weather_viewer.dao;
+
+import me.vladislav.weather_viewer.models.User;
+import me.vladislav.weather_viewer.utils.HibernateUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserDAO implements DataAccessObject<User> {
+
+    public Optional<User> getByEmail(String email){
+        try(Session session = HibernateUtils.getSession()){
+            session.beginTransaction();
+            Query<User> query = session.createQuery("SELECT u FROM User u WHERE u.login=:login", User.class);
+            query.setParameter("login", email);
+            User user = query.uniqueResult();
+            session.getTransaction().commit();
+            return Optional.ofNullable(user);
+        }catch(HibernateException e){
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<User> getById(int id) {
+        try (Session session = HibernateUtils.getSession()) {
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.getTransaction().commit();
+            return Optional.ofNullable(user);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<List<User>> getAll() {
+        try (Session session = HibernateUtils.getSession()) {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("SELECT u FROM User u", User.class);
+            List<User> listOfUsers = query.getResultList();
+            session.getTransaction().commit();
+            return Optional.ofNullable(listOfUsers);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void save(User user) {
+        try (Session session = HibernateUtils.getSession()) {
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(User user) {
+        try (Session session = HibernateUtils.getSession()) {
+            session.beginTransaction();
+            session.remove(user);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
 }
