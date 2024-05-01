8acba2624af17d09429dad8d976c78c8de5b9604
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-01 21:34:43 +0300

    refactor: updated LocationDAO.java, DataAccessObject.java

--- src/main/java/me/vladislav/weather_viewer/dao/DataAccessObject.java
+++ src/main/java/me/vladislav/weather_viewer/dao/DataAccessObject.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 
 public interface DataAccessObject<T> {
     public Optional<T> getById(int id);
+
     public Optional<List<T>> getAll();
+
     public void save(T t);
+
     public void delete(T t);
 }
--- src/main/java/me/vladislav/weather_viewer/dao/LocationDAO.java
+++ src/main/java/me/vladislav/weather_viewer/dao/LocationDAO.java
@@ -1,2 +1,61 @@
-package me.vladislav.weather_viewer.dao;public class LocationDAO {
+package me.vladislav.weather_viewer.dao;
+
+import me.vladislav.weather_viewer.models.Location;
+import me.vladislav.weather_viewer.utils.HibernateUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public class LocationDAO implements DataAccessObject<Location> {
+    @Override
+    public Optional<Location> getById(int id) {
+        try (Session session = HibernateUtils.getSession()) {
+            session.beginTransaction();
+            Location location = session.get(Location.class, id);
+            session.getTransaction().commit();
+            return Optional.ofNullable(location);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public Optional<List<Location>> getAll() {
+        try (Session session = HibernateUtils.getSession()) {
+            session.beginTransaction();
+            Query<Location> query = session.createQuery("SELECT l FROM Location l", Location.class);
+            List<Location> listOfLocation = query.getResultList();
+            session.getTransaction().commit();
+            return Optional.ofNullable(listOfLocation);
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void save(Location location) {
+        try (Session session = HibernateUtils.getSession()) {
+            session.beginTransaction();
+            session.persist(location);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Location location) {
+        try (Session session = HibernateUtils.getSession()) {
+            session.beginTransaction();
+            session.remove(location);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
 }
