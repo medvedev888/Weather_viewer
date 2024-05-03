9f031d1f5475dd1e7d08080be1aa15ead386d94a
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-03 21:50:40 +0300

    feat: added DataAccessException.java;

---
+++ src/main/java/me/vladislav/weather_viewer/exceptions/DataAccessException.java
@@ -0,0 +1,13 @@
+package me.vladislav.weather_viewer.exceptions;
+
+public class DataAccessException extends RuntimeException {
+
+    public DataAccessException(String message) {
+        super(message);
+    }
+
+    public DataAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
