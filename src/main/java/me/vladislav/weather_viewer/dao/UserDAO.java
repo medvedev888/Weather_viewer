f3c00fa75ace86b7bbcaa580437095f345692e23
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-03 21:51:57 +0300

    refactor: updated UserDAO.java;
    added logging;

--- src/main/java/me/vladislav/weather_viewer/dao/UserDAO.java
+++ src/main/java/me/vladislav/weather_viewer/dao/UserDAO.java
@@ -1,5 +1,7 @@
 package me.vladislav.weather_viewer.dao;
 
+import lombok.extern.slf4j.Slf4j;
+import me.vladislav.weather_viewer.exceptions.DataAccessException;
 import me.vladislav.weather_viewer.models.User;
 import me.vladislav.weather_viewer.utils.HibernateUtils;
 import org.hibernate.HibernateException;
@@ -9,9 +11,11 @@ import org.hibernate.query.Query;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 public class UserDAO implements DataAccessObject<User> {
 
     public Optional<User> getByLogin(String login) {
+        log.info("Start get user by the login");
         try (Session session = HibernateUtils.getSession()) {
             session.beginTransaction();
             Query<User> query = session.createQuery("SELECT u FROM User u WHERE u.login=:login", User.class);
@@ -20,26 +24,26 @@ public class UserDAO implements DataAccessObject<User> {
             session.getTransaction().commit();
             return Optional.ofNullable(user);
         } catch (HibernateException e) {
-            e.printStackTrace();
-            return Optional.empty();
+            throw new DataAccessException("Error when getting user by login", e);
         }
     }
 
     @Override
     public Optional<User> getById(int id) {
+        log.info("Start get user by the id");
         try (Session session = HibernateUtils.getSession()) {
             session.beginTransaction();
             User user = session.get(User.class, id);
             session.getTransaction().commit();
             return Optional.ofNullable(user);
         } catch (HibernateException e) {
-            e.printStackTrace();
-            return Optional.empty();
+            throw new DataAccessException("Error when getting user by id", e);
         }
     }
 
     @Override
     public Optional<List<User>> getAll() {
+        log.info("Start get list of users");
         try (Session session = HibernateUtils.getSession()) {
             session.beginTransaction();
             Query<User> query = session.createQuery("SELECT u FROM User u", User.class);
@@ -47,30 +51,31 @@ public class UserDAO implements DataAccessObject<User> {
             session.getTransaction().commit();
             return Optional.ofNullable(listOfUsers);
         } catch (HibernateException e) {
-            e.printStackTrace();
-            return Optional.empty();
+            throw new DataAccessException("Error when getting list of users", e);
         }
     }
 
     @Override
     public void save(User user) {
+        log.info("Start save user");
         try (Session session = HibernateUtils.getSession()) {
             session.beginTransaction();
             session.persist(user);
             session.getTransaction().commit();
         } catch (HibernateException e) {
-            e.printStackTrace();
+            throw new DataAccessException("Error when saving user", e);
         }
     }
 
     @Override
     public void delete(User user) {
+        log.info("Start delete user");
         try (Session session = HibernateUtils.getSession()) {
             session.beginTransaction();
             session.remove(user);
             session.getTransaction().commit();
         } catch (HibernateException e) {
-            e.printStackTrace();
+            throw new DataAccessException("Error when deleting user", e);
         }
     }
 }
