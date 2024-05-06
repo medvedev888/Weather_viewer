940e65696fa0c459a63e7acbd1adf6dae5084a16
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-07 01:54:16 +0300

    feat: added doGet() method in HomeServlet.java, updated BaseServlet.java;

--- src/main/java/me/vladislav/weather_viewer/controllers/BaseServlet.java
+++ src/main/java/me/vladislav/weather_viewer/controllers/BaseServlet.java
@@ -1,6 +1,5 @@
 package me.vladislav.weather_viewer.controllers;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,13 +19,14 @@ public class BaseServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             super.service(req, resp);
-        } catch(CookieNotFoundException | SessionExpiredException e){
+        } catch (CookieNotFoundException | SessionExpiredException e) {
             log.warn(e.getMessage());
             resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
             WebContext webContext = ThymeleafUtils.getWebContext(req, resp, getServletContext());
             TemplateEngine templateEngine = (TemplateEngine) (getServletContext().getAttribute("templateEngine"));
             webContext.clearVariables();
+            // вынести в отдельную функцию (настройки для неавторизованных пользователей)
             webContext.setVariable("showHomeLink", false);
             webContext.setVariable("showSearchLink", false);
             webContext.setVariable("showRegistrationLink", true);
--- src/main/java/me/vladislav/weather_viewer/controllers/HomeServlet.java
+++ src/main/java/me/vladislav/weather_viewer/controllers/HomeServlet.java
@@ -1,10 +1,18 @@
 package me.vladislav.weather_viewer.controllers;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import me.vladislav.weather_viewer.dao.SessionDAO;
+import me.vladislav.weather_viewer.exceptions.CookieNotFoundException;
+import me.vladislav.weather_viewer.exceptions.SessionExpiredException;
+import me.vladislav.weather_viewer.models.Session;
+import me.vladislav.weather_viewer.models.User;
+import me.vladislav.weather_viewer.utils.CookieUtils;
+import me.vladislav.weather_viewer.utils.SessionUtils;
 import me.vladislav.weather_viewer.utils.ThymeleafUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -12,12 +20,43 @@ import org.thymeleaf.context.WebContext;
 import java.io.IOException;
 
 @WebServlet(name = "HomeServlet", value = "/")
-public class HomeServlet extends HttpServlet {
+public class HomeServlet extends BaseServlet {
+    private SessionDAO sessionDAO;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        ServletContext servletContext = getServletContext();
+        sessionDAO = (SessionDAO) servletContext.getAttribute("sessionDAO");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         WebContext webContext = ThymeleafUtils.getWebContext(req, resp, getServletContext());
-
         TemplateEngine templateEngine = (TemplateEngine) (getServletContext().getAttribute("templateEngine"));
+
+        Cookie[] cookies = req.getCookies();
+        Cookie cookie = CookieUtils.findCookie(cookies, "sessionId").orElseThrow(() -> new CookieNotFoundException("Cookie not found"));
+
+        Session session = sessionDAO.getById(Integer.parseInt(cookie.getValue())).orElseThrow(() -> new SessionExpiredException("Session has expired"));
+
+        if (SessionUtils.isSessionExpired(session)) {
+            throw new SessionExpiredException("Session has expired");
+        }
+
+        User user = session.getUser();
+
+        //вывод локаций для пользователя
+
+        // вынести в отдельную функцию (настройки для авторизованных пользователей)
+        webContext.setVariable("showHomeLink", false);
+        webContext.setVariable("showSearchLink", true);
+        webContext.setVariable("showRegistrationLink", false);
+        webContext.setVariable("showAuthorizationLink", false);
+        webContext.setVariable("showSignOutLink", true);
+
+        //для проверки работы
+        webContext.setVariable("userName", user.getLogin());
 
         templateEngine.process("home.html", webContext, resp.getWriter());
     }
