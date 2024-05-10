89676b014f364de7f6dd8fbaf431b5efbf4e9720
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-10 21:23:13 +0300

    feat: added search.html and SearchServlet.java;

---
+++ src/main/java/me/vladislav/weather_viewer/controllers/SearchServlet.java
@@ -0,0 +1,65 @@
+package me.vladislav.weather_viewer.controllers;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import me.vladislav.weather_viewer.dao.SessionDAO;
+import me.vladislav.weather_viewer.exceptions.CookieNotFoundException;
+import me.vladislav.weather_viewer.exceptions.SessionExpiredException;
+import me.vladislav.weather_viewer.models.Session;
+import me.vladislav.weather_viewer.models.User;
+import me.vladislav.weather_viewer.utils.CookieUtils;
+import me.vladislav.weather_viewer.utils.SessionUtils;
+import me.vladislav.weather_viewer.utils.ThymeleafUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import java.io.IOException;
+
+@WebServlet(name = "SearchServlet", value = "/search")
+public class SearchServlet extends BaseServlet {
+    private SessionDAO sessionDAO;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        ServletContext servletContext = getServletContext();
+        sessionDAO = (SessionDAO) servletContext.getAttribute("sessionDAO");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebContext webContext = ThymeleafUtils.getWebContext(req, resp, getServletContext());
+        TemplateEngine templateEngine = (TemplateEngine) (getServletContext().getAttribute("templateEngine"));
+
+        Cookie[] cookies = req.getCookies();
+        Cookie cookie = CookieUtils.findCookie(cookies, "sessionId").orElseThrow(() -> new CookieNotFoundException("Cookie not found"));
+
+        Session session = sessionDAO.getById(Integer.parseInt(cookie.getValue())).orElseThrow(() -> (new SessionExpiredException("Session has expired")));
+
+        if (SessionUtils.isSessionExpired(session)) {
+            sessionDAO.delete(session);
+            throw new SessionExpiredException("Session has expired");
+        }
+
+        User user = session.getUser();
+
+        //вывод локаций для пользователя
+
+        setTemplateVariablesForAuthenticatedUsers(webContext, true, false);
+
+        //для проверки работы
+        webContext.setVariable("userName", user.getLogin());
+
+        templateEngine.process("home.html", webContext, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
