8fd3bcd37d4b7bc371c49517eaacff7e06e74752
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-10 23:26:40 +0300

    refactor: updated ValidationUtils.java;

--- src/main/java/me/vladislav/weather_viewer/utils/ValidationUtils.java
+++ src/main/java/me/vladislav/weather_viewer/utils/ValidationUtils.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ValidationUtils {
 
+    private static String regex = "^[a-zA-Z0-9\\-_.$@%&*+!?()\\[\\]{}=:/;,#'\\\"`\\\\]*$";
+
+
     public static boolean isValidLogin(String login) {
         log.info("Validating login");
-        String regex = "^[a-zA-Z0-9\\-_.$@%&*+!?()\\[\\]{}=:/;,#'\\\"`\\\\]*$";
 
         if (login == null || login.isEmpty()) {
             return false;
@@ -20,13 +22,24 @@ public class ValidationUtils {
 
     public static boolean isValidPassword(String password) {
         log.info("Validating password");
-        String regex = "^[a-zA-Z0-9\\-_.$@%&*+!?()\\[\\]{}=:/;,#'\\\"`\\\\]*$";
 
         if (password == null || password.isEmpty()) {
             return false;
         }
         if (password.length() >= 8 && password.length() <= 64) {
             return password.matches(regex);
+        }
+        return false;
+    }
+
+    public static boolean isValidLocationName(String locationName){
+        log.info("Validating location name");
+
+        if (locationName == null || locationName.isEmpty()) {
+            return false;
+        }
+        if (locationName.length() <= 255) {
+            return locationName.matches(regex);
         }
         return false;
     }
