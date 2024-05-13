a501cc09e5d1e37ed69ac6a6817000e4dd907de5
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-13 19:35:21 +0300

    feat: added WeatherAPIService.java;

---
+++ src/main/java/me/vladislav/weather_viewer/exceptions/WeatherApiException.java
@@ -0,0 +1,11 @@
+package me.vladislav.weather_viewer.exceptions;
+
+public class WeatherApiException extends RuntimeException {
+    public WeatherApiException(String message) {
+        super(message);
+    }
+
+    public WeatherApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
