50a6eafe0a2be6a4cf0169387ba0a0680f7b966c
vladislavmedvedev <EnoT3581@mail.ru>
2024-05-13 22:19:16 +0300

    refactor: updated home.html, HomeServlet.java, WeatherAPIService.java;
    added city weather display (initial version, needs improvement);

--- src/main/java/me/vladislav/weather_viewer/controllers/HomeServlet.java
+++ src/main/java/me/vladislav/weather_viewer/controllers/HomeServlet.java
@@ -8,11 +8,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import me.vladislav.weather_viewer.dao.LocationDAO;
 import me.vladislav.weather_viewer.dao.SessionDAO;
+import me.vladislav.weather_viewer.dto.WeatherDTO;
 import me.vladislav.weather_viewer.exceptions.CookieNotFoundException;
 import me.vladislav.weather_viewer.exceptions.SessionExpiredException;
 import me.vladislav.weather_viewer.models.Location;
 import me.vladislav.weather_viewer.models.Session;
 import me.vladislav.weather_viewer.models.User;
+import me.vladislav.weather_viewer.services.WeatherAPIService;
 import me.vladislav.weather_viewer.utils.CookieUtils;
 import me.vladislav.weather_viewer.utils.SessionUtils;
 import me.vladislav.weather_viewer.utils.ThymeleafUtils;
@@ -20,12 +22,14 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "HomeServlet", value = "/")
 public class HomeServlet extends BaseServlet {
     private SessionDAO sessionDAO;
     private LocationDAO locationDAO;
+    private WeatherAPIService weatherAPIService;
 
     @Override
     public void init() throws ServletException {
@@ -33,6 +37,7 @@ public class HomeServlet extends BaseServlet {
         ServletContext servletContext = getServletContext();
         sessionDAO = (SessionDAO) servletContext.getAttribute("sessionDAO");
         locationDAO = (LocationDAO) servletContext.getAttribute("locationDAO");
+        weatherAPIService = (WeatherAPIService) servletContext.getAttribute("weatherAPIService");
     }
 
     @Override
@@ -52,9 +57,15 @@ public class HomeServlet extends BaseServlet {
 
         User user = session.getUser();
 
-        List<Location> listOfLocations = locationDAO.getLocationsByTheUser(user).orElseGet(() -> null);
-        // добавить прогноз погоды
+        List<Location> listOfLocations = locationDAO.getLocationsByTheUser(user).orElseGet(ArrayList::new);
+        List<WeatherDTO> listOfWeathers = new ArrayList<>();
+
+        for (Location location : listOfLocations) {
+            listOfWeathers.add(weatherAPIService.getWeatherByTheLocation(location));
+        }
+
         webContext.setVariable("listOfLocations", listOfLocations);
+        webContext.setVariable("listOfWeathers", listOfWeathers);
 
         setTemplateVariablesForAuthenticatedUsers(webContext, false, true);
 
--- src/main/java/me/vladislav/weather_viewer/services/WeatherAPIService.java
+++ src/main/java/me/vladislav/weather_viewer/services/WeatherAPIService.java
@@ -3,9 +3,10 @@ package me.vladislav.weather_viewer.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.vladislav.weather_viewer.dto.LocationDTO;
+import me.vladislav.weather_viewer.dto.WeatherDTO;
 import me.vladislav.weather_viewer.exceptions.WeatherApiException;
+import me.vladislav.weather_viewer.models.Location;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -15,11 +16,11 @@ import java.util.List;
 public class WeatherAPIService {
     private static final String API_KEY = "578e5a198c9d27a1f372eec093d98159";
     private static final String API_URL = "https://api.openweathermap.org";
-    private static final String API_URL_GEO_SUFFIX = "/geo/1.0/direct";
+    private static final String API_URL_GEOCODING_SUFFIX = "/geo/1.0/direct";
+    private static final String API_URL_WEATHER_SUFFIX = "/data/2.5/weather";
 
     private final HttpClient client = HttpClient.newHttpClient();
     private final ObjectMapper objectMapper = new ObjectMapper();
-
 
     public List<LocationDTO> getLocationsByTheName(String locationName) {
         try {
@@ -28,8 +29,20 @@ public class WeatherAPIService {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
             return objectMapper.readValue(response.body(), new TypeReference<List<LocationDTO>>() {});
-        } catch (IOException | InterruptedException e) {
+        } catch (Exception e) {
             throw new WeatherApiException("Error when retrieving location by the name", e);
+        }
+    }
+
+    public WeatherDTO getWeatherByTheLocation(Location location) {
+        try {
+            URI uri = buildUriForWeatherRequest(location);
+            HttpRequest request = HttpRequest.newBuilder(uri).GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return objectMapper.readValue(response.body(), new TypeReference<WeatherDTO>() {});
+        } catch (Exception e) {
+            throw new WeatherApiException("Error when retrieving weather by the location", e);
         }
     }
 
@@ -39,4 +52,13 @@ public class WeatherAPIService {
                 "&appid=" + API_KEY +
                 "&units=metric");
     }
+
+    private static URI buildUriForWeatherRequest(Location location) {
+        return URI.create(API_URL + API_URL_WEATHER_SUFFIX +
+                "?lat=" + location.getLatitude() +
+                "&lon=" + location.getLongitude() +
+                "&appid=" + API_KEY +
+                "&units=" + "metric");
+    }
+
 }
